fa9d4bddf5277ac88509bef31c20dca46eb00a97
Chris Pressland <cpressland@bink.com>
2019-09-03 13:50:57 +0000

    Update settings.py

--- zagreus/settings.py
+++ zagreus/settings.py
@@ -145,8 +145,8 @@ LOGGING = {
 
 # AMQP connection details
 AMQP_QUEUE = env_var("AMQP_QUEUE", "auth-transactions")
-AMQP_USER = env_var("AMQP_QUEUE", "guest")
-AMQP_PASSWORD = env_var("AMQP_QUEUE", "guest")
-AMQP_HOST = env_var("AMQP_QUEUE", "localhost")
+AMQP_USER = env_var("AMQP_USER", "guest")
+AMQP_PASSWORD = env_var("AMQP_PASSWORD", "guest")
+AMQP_HOST = env_var("AMQP_HOST", "localhost")
 AMQP_PORT = env_var("AMQP_PORT", "5672")
 AMQP_DSN = f"amqp://{AMQP_USER}:{AMQP_PASSWORD}@{AMQP_HOST}:{AMQP_PORT}//"
